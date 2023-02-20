0a179e769d8255bbf8db9ef3729cc356ea3ec9f5
jaimin kapopara <88267997+Jaiminkapopara@users.noreply.github.com>
2023-02-20 18:48:23 +0530

    1

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Topbar = () => {
+  return (
+    <div className='topbar'>
+      <div className="topbarWrapper">
+        <div className="topLeft">l</div>
+        <div className="topRight">r</div>
+      </div>
+    </div>
+  )
+}
+
+export default Topbar
